f6d40ea7934fb293ca04b6b7e030e44361e1d7be
HamedMiri72 <hamedmiri72@gmail.com>
2025-03-29 14:47:16 +0000

    side bar using shadcn

--- src/app/page.js
+++ src/app/page.js
@@ -11,19 +11,7 @@ import {
 export default function Home() {
   return (
     <div className='m-4 flex items-center'>
-      <SignedOut>
-        <SignInButton mode='modal'>
-          <Button>Sign in </Button>
-        </SignInButton>
-      </SignedOut>
-
-
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-
-      <Button variant={"secondary"}>click me </Button>
-      <ModeToggle/>
+     
     </div>
   );
 }
